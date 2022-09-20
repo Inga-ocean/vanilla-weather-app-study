7c9d66fd4e8ec9bc9112fd45e7820156e24bc3c6
Inga-ocean <112891200+Inga-ocean@users.noreply.github.com>
2022-09-20 15:46:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "5863935ee9cca4c02ed68203f807c65b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
